db744a2e23a8555d50f7904133563471dcf97366
Dimitri Rybak <dima_rybak@yahoo.de>
2024-07-27 14:59:41 +0200

    changes

--- app/src/main/java/com/dantsu/thermalprinter/Constants.java
+++ app/src/main/java/com/dantsu/thermalprinter/Constants.java
@@ -3,5 +3,5 @@ package com.dantsu.thermalprinter;
 public class Constants {
     public static boolean isServiceActive = false;
     public static String APP_DETAILS_URL = "https://app-version.jandiweb.de/printer-app/app-details.json";
-    public static String currentAppVersion = "2.0.0";
+    public static String currentAppVersion = "1.1.0";
 }
