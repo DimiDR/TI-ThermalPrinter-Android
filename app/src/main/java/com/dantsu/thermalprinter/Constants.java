9fd361e48a3ab50891bf4696f7b3cf438feb47d5
Dimitri Rybak <dima_rybak@yahoo.de>
2025-11-08 08:45:33 +0100

    changes

--- app/src/main/java/com/dantsu/thermalprinter/Constants.java
+++ app/src/main/java/com/dantsu/thermalprinter/Constants.java
@@ -3,7 +3,7 @@ package com.dantsu.thermalprinter;
 public class Constants {
     public static boolean isServiceActive = false;
     public static String APP_DETAILS_URL = "https://app-version.jandiweb.de/printer-app/app-details.json";
-    public static String currentAppVersion = "1.0.9";
+    public static String currentAppVersion = "1.1.0";
     
     // Refresh interval for order monitoring when app is in foreground
     public static final long REFRESH_INTERVAL_KITCHEN_OPEN = 60000; // 1 minute when app is in foreground
